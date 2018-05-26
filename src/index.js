7b9278d2def62cfda5bec48f75309c8de037b85e
Justen Robertson <justen@justen.us>
2018-05-26 15:04:36 -0700

    fix: various bugs found in testing

--- src/index.js
+++ src/index.js
@@ -12,12 +12,12 @@ function vallocFactory(pool, init, clean) {
   let valloc = {}
   let used = 0
   Object.defineProperties(valloc, {
-    next: allocate.bind(undefined, pool, allocator, init, used),
-    nextIndex: nextIndex.bind(undefined, allocator),
-    indexOf: pool.indexOf.bind(allocator),
-    isAllocated: isAllocated.bind(undefined, pool, allocator),
-    isIndexAllocated: isIndexAllocated.bind(undefined, allocator),
-    free: free.bind(undefined, pool, allocator, clean, used),
+    next: {value: allocate.bind(undefined, pool, allocator, init, used)},
+    indexOf: {value: pool.indexOf.bind(allocator)},
+    isAllocated: {value: isAllocated.bind(undefined, pool, allocator)},
+    isIndexAllocated: {value: isIndexAllocated.bind(undefined, allocator)},
+    free: {value: free.bind(undefined, pool, allocator, clean, used)},
+    nextIndex: {get: nextIndex.bind(undefined, allocator)},
     length: {get: () => allocator.length},
     available: {get: () => allocator.length - used},
     used: {get: () => used}
@@ -31,15 +31,19 @@ function vallocFactory(pool, init, clean) {
  * @param
  */
 export const from = (array) => {
-  if (typeof array === undefined) {throw new Error("valloc.from requires an array")}
-  vallocFactory(array)
+  if (typeof array === "undefined") {
+    throw new Error("valloc.from requires an array")
+  }
+  return vallocFactory(array)
 }
 
 /**
  * Create a new allocator with an internally managed pool
  */
 export function create(length, opts = {}) {
-  if (!length) {throw new Error("valloc.create requires a length")}
+  if (!length) {
+    throw new Error("valloc.create requires a length")
+  }
   length = ~~length; // enforce integer
   const factory = opts.factory || defaultCreateOpts.factory
   const init = opts.init || defaultCreateOpts.init
