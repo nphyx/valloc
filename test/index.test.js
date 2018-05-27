40446ec212e06b4d581df55758d70a5f1cd1ab98
Justen Robertson <justen@justen.us>
2018-05-26 17:53:27 -0700

    test: for index, coverage for utils

---
+++ test/index.test.js
@@ -0,0 +1,121 @@
+import * as valloc from "../src/"
+
+const allocatorProperties = {
+  next: "function",
+  indexOf: "function",
+  isAllocated: "function",
+  isIndexAllocated: "function",
+  free: "function",
+  nextIndex: "number",
+  length: "number",
+  available: "number",
+  used: "number"
+}
+
+describe("valloc.from", () => {
+  it("should require an array as the first parameter", () => {
+    (() => valloc.from()).should.throw()
+  })
+  it("should create a valloc allocator from an array", () => {
+    let array = new Array(1)
+    let allocator = valloc.from(array)
+    for (let key of Object.getOwnPropertyNames(allocatorProperties)) {
+      allocator[key].should.have.type(allocatorProperties[key])
+    }
+  })
+})
+
+describe("valloc.create", () => {
+  it("should require length as the first parameter", () => {
+    (() => valloc.create()).should.throw()
+  })
+  it("should create a valloc allocator", () => {
+    let allocator = valloc.create(1)
+    for (let key of Object.getOwnPropertyNames(allocatorProperties)) {
+      allocator[key].should.have.type(allocatorProperties[key])
+    }
+  })
+})
+
+describe("valloc allocators", () => {
+  let allocator, member, count, length
+  before(() => {
+    count = 0
+    length = 10
+    const opts = {
+      factory: (i) => ({index: i, foo: "bar"}),
+      init: (m, string = "bar") => m.foo = string,
+      clean: (m) => m.foo = "bar"
+    }
+    allocator = valloc.create(length, opts)
+  })
+  it("should implement length as a getter", () => {
+    allocator.length.should.eql(length);
+    (() => allocator.length = 11).should.throw()
+  })
+  it("should implement nextIndex as a getter", () => {
+    allocator.nextIndex.should.eql(0);
+    (() => allocator.nextIndex = 9).should.throw()
+  })
+  it("should implement available as a getter", () => {
+    allocator.available.should.eql(length);
+    (() => allocator.available = 9).should.throw()
+  })
+  it("should implement used as a getter", () => {
+    allocator.used.should.eql(0);
+    (() => allocator.used = 10).should.throw()
+  })
+  it("should allocate a new member", () => {
+    member = allocator.next()
+    count++
+    member.should.have.properties(["index", "foo"])
+    member.index.should.eql(0)
+    member.foo.should.eql("bar")
+  })
+  it("should support params during initialization", () => {
+    member = allocator.next("baz")
+    count++
+    member.index.should.eql(1)
+    member.foo.should.eql("baz")
+  })
+  it("should report that an index is allocated", () => {
+    for (let i = 0; i < count; ++i) {
+      allocator.isIndexAllocated(i).should.be.true()
+    }
+    allocator.isIndexAllocated(count).should.be.false()
+  })
+  it("should report that an object is allocated", () => {
+    allocator.isAllocated(member).should.be.true()
+  })
+  it("should report the index of an allocated object", () => {
+    allocator.indexOf(member).should.eql(member.index)
+  })
+  it("should free an existing member", () => {
+    member = allocator.next()
+    let index = member.index
+    allocator.free(member)
+    allocator.isIndexAllocated(index).should.be.false()
+  })
+  it("should update used and available after free", () => {
+    allocator.used.should.eql(count)
+    allocator.available.should.eql(length - count)
+  })
+  it("should free an existing member by index", () => {
+    let index = count - 1
+    allocator.freeIndex(index)
+    count--
+    allocator.isIndexAllocated(index).should.be.false()
+  })
+  it("should update used and available after freeIndex", () => {
+    allocator.used.should.eql(count)
+    allocator.available.should.eql(length - count)
+  })
+  it("should supply default factory, init and clean functions", () => {
+    // this would throw if there was no default factory
+    let allocator = valloc.create(10)
+    // this would throw if there was no default init
+    let member = allocator.next()
+    // this would throw if there was no default clean
+    allocator.free(member)
+  })
+})
--- test/util.test.js
+++ test/util.test.js
@@ -1,17 +1,164 @@
 import * as util from "../src/util"
 
 /* global describe, xit, it, before */
+describe("util.nextIndex", () => {
+  let allocator
+  before(() => {
+    allocator = [true,true,false,true,false,true,false,false]
+  })
+  it("should find the next avialable index", () => {
+    util.nextIndex(allocator).should.eql(2)
+    allocator[2] = true
+    util.nextIndex(allocator).should.eql(4)
+  })
+  it("should return -1 if the pool is full", () => {
+    allocator.fill(true)
+    util.nextIndex(allocator).should.eql(-1)
+  })
+})
+
+describe("util.allocateIndex", () => {
+  let pool, allocator, init, index, length
+  before(() => {
+    index = 0
+    length = 3
+    pool = new Array(length)
+    allocator = new Array(length)
+    allocator.fill(false)
+    init = (m, string = "baz", also = "nope") => {
+      m.foo = string
+      m.also = also
+    }
+    for (let i = 0; i < length; ++i) {
+      pool[i] = {foo: "bar"}
+    }
+  })
+  it("should allocate a new pool member", () => {
+    let member = util.allocateIndex(pool, allocator, () => {}, index)
+    allocator[index].should.be.true()
+    member.foo.should.eql("bar");
+    (typeof member.also).should.eql("undefined")
+    pool.indexOf(member).should.eql(index)
+    index++
+  })
+  it("should use a supplied init function", () => {
+    let member = util.allocateIndex(pool, allocator, init, index)
+    allocator[index].should.be.true()
+    member.foo.should.eql("baz")
+    member.also.should.eql("nope")
+    pool.indexOf(member).should.eql(index)
+    index++
+  })
+  it("should pass through init arguments", () => {
+    let member = util.allocate(pool, allocator, init, "qux", "yup")
+    allocator[index].should.be.true()
+    member.foo.should.eql("qux")
+    member.also.should.eql("yup")
+    pool.indexOf(member).should.eql(index)
+  })
+  it("should complain when the index is already allocated", () => {
+    (() => util.allocateIndex(pool, allocator, init, index)).should.throw()
+  })
+  it("should complain when the index is out of bounds", () => {
+    (() => util.allocateIndex(pool, allocator, init, length)).should.throw()
+  })
+})
+
 describe("util.allocate", () => {
-  xit("should allocate a new pool member", () => {})
-  xit("should use a supplied init function", () => {})
-  xit("should complain when the pool is full", () => {})
+  let pool, allocator, init, count, length
+  before(() => {
+    count = 0
+    length = 3
+    pool = new Array(length)
+    allocator = new Array(length)
+    allocator.fill(false)
+    init = (m, string = "baz") => m.foo = string
+    for (let i = 0; i < length; ++i) {
+      pool[i] = {foo: "bar"}
+    }
+  })
+  it("should allocate a new pool member", () => {
+    let member = util.allocate(pool, allocator, () => {})
+    allocator[count].should.be.true()
+    member.foo.should.eql("bar")
+    pool.indexOf(member).should.eql(count)
+    count++
+  })
+  it("should use a supplied init function", () => {
+    let member = util.allocate(pool, allocator, init)
+    allocator[count].should.be.true()
+    member.foo.should.eql("baz")
+    pool.indexOf(member).should.eql(count)
+    count++
+  })
+  it("should pass through init arguments", () => {
+    let member = util.allocate(pool, allocator, init, "qux")
+    allocator[count].should.be.true()
+    member.foo.should.eql("qux")
+    pool.indexOf(member).should.eql(count)
+    count++
+  })
+  it("should complain when the pool is full", () => {
+    (() => util.allocate(pool, allocator)).should.throw()
+  })
+})
+
+describe("utl.freeIndex", () => {
+  let pool, allocator, clean, count, length
+  before(() => {
+    length = 2
+    count = length
+    pool = new Array(length)
+    allocator = new Array(length)
+    allocator.fill(true)
+    clean = (m) => m.foo = "baz"
+    for (let i = 0; i < length; ++i) {
+      pool[i] = {foo: "bar"}
+    }
+  })
+  it("should free a member", () => {
+    count--
+    util.freeIndex(pool, allocator, () => {}, count)
+    allocator[count].should.be.false()
+  })
+  it("should use a supplied clean function", () => {
+    count--
+    util.freeIndex(pool, allocator, clean, count)
+    pool[count].foo.should.eql("baz")
+    allocator[count].should.be.false()
+  })
+  it("should complain when the member is already free", () => {
+    (() => util.freeIndex(pool, allocator, () => {}, count)).should.throw()
+  })
 })
 
 describe("utl.free", () => {
-  xit("should free a member", () => {})
-  xit("should use a supplied clean function", () => {})
-  xit("should complain when the argument is not a member", () => {})
-  xit("should complain when the member is already free", () => {})
+  let pool, allocator, clean, count, length
+  before(() => {
+    length = 2
+    count = length
+    pool = new Array(length)
+    allocator = new Array(length)
+    allocator.fill(true)
+    clean = (m) => m.foo = "baz"
+    for (let i = 0; i < length; ++i) {
+      pool[i] = {foo: "bar"}
+    }
+  })
+  it("should free a member", () => {
+    count--
+    util.free(pool, allocator, () => {}, pool[count])
+    allocator[count].should.be.false()
+  })
+  it("should use a supplied clean function", () => {
+    count--
+    util.free(pool, allocator, clean, pool[count])
+    pool[count].foo.should.eql("baz")
+    allocator[count].should.be.false()
+  })
+  it("should complain when the argument is not a member", () => {
+    (() => util.free(pool, allocator, clean, {foo: "qux"})).should.throw()
+  })
 })
 
 describe("util.isAllocated", () => {
@@ -50,18 +197,4 @@ describe("util.isIndexAllocated", () => {
   })
 })
 
-describe("util.nextIndex", () => {
-  let allocator
-  before(() => {
-    allocator = [true,true,false,true,false,true,false,false]
-  })
-  it("should find the next avialable index", () => {
-    util.nextIndex(allocator).should.eql(2)
-    allocator[2] = true
-    util.nextIndex(allocator).should.eql(4)
-  })
-  it("should return -1 if the pool is full", () => {
-    allocator.fill(true)
-    util.nextIndex(allocator).should.eql(-1)
-  })
-})
+
